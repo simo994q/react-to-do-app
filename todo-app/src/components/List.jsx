b796eee26f9367dc2541bea44f71e773e35cd0d0
simo994q <simonkn1@hotmail.com>
2023-08-17 14:47:27 +0200

    remove func

--- todo-app/src/components/List.jsx
+++ todo-app/src/components/List.jsx
@@ -1,33 +1,49 @@
 import style from './List.module.scss'
 import { useState } from 'react'
 
-export function List () {
+export function List() {
 
-    const [itemArray, setArray] = useState(['opgave 1'])
+    const [itemArray, setArray] = useState([])
 
     const [newItem, addItem] = useState('')
 
-    return (
-        <div className={style.container}>
-            <h1>To Do</h1>
+    const removeItem = (index) => {
+        const updatedArray = itemArray.filter((_, i) => i !== index);
+        setArray(updatedArray);
+      };
 
-            <div className={style.itemsContainer}>
-                <table>
-                    {itemArray.map((item, index) => {
-                        return (
-                            <tr key={index}>
-                                <td>{item}</td>
-                                <td>remove</td>
-                            </tr>
-                        )
-                    })}
-                </table>
+    return (
+        <div className={style.fullContainer}>
+            <div className={style.container}>
+                <h1>To Do</h1>
+
+                <div className={style.itemsContainer}>
+                    <table>
+                        <tbody>
+                            {itemArray.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{item}</td>
+                                        <td onClick={() => {removeItem(index)}}>remove</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+                <form
+                    className={style.newItemContainer}
+                    onSubmit={(event) => {
+                        event.preventDefault(); // Prevent the default form submission behavior
+                        setArray([...itemArray, newItem]); // Update the array with the new item
+                        addItem('');
+                    }}
+                >
+                    <input onChange={(event) => { addItem(event.target.value)}} value={newItem} />
+                    <input type='submit' value='+ Add new' />
+                </form>
+
             </div>
-            <form className={style.newItemContainer} onSubmit={setArray(newItem)}>
-                <input onChange={(event) => {addItem(event.target.value)}}/>
-                <input type='submit' value='+ Add ned'></input>
-            </form>
-        
-        </div>  
+        </div>
     )
 }
