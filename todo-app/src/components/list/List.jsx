e4ed31f7909a02a213c32ddc9cec46756d15537e
simo994q <simonkn1@hotmail.com>
2023-08-18 13:09:02 +0200

    yippeeee

--- todo-app/src/components/list/List.jsx
+++ todo-app/src/components/list/List.jsx
@@ -6,13 +6,36 @@ export function List() {
     const [itemArray, setArray] = useState([])
     const [newItem, addItem] = useState('')
 
-    const removeItem = (index) => {
-        const updatedArray = itemArray.filter((_, i) => i !== index);
-        setArray(updatedArray);
-    };
+    const [progressArray, setProgressArray] = useState([])
+
+    const [doneArray, setDoneArray] = useState([])
+
+
+    const removeItem = (index, array, arrayNumber) => {
+        const updatedArray = array.filter((_, i) => i !== index);
+
+        if (arrayNumber == 1) {
+            setArray(updatedArray);
+        }
+        if (arrayNumber == 2) {
+            setProgressArray(updatedArray);
+        }
+        if (arrayNumber == 3) {
+            setDoneArray(updatedArray);
+        }
+    }
+    
+    const moveItem = (fromArray, toArray, index, setArrayFrom, setArrayTo) => {
+        const movedItem = fromArray.filter((_, i) => i == index);
+        setArrayTo([...toArray, movedItem])
+        const updatedArray = fromArray.filter((_, i) => i !== index);
+        setArrayFrom(updatedArray)
+    }
 
     const deleteAll = () => {
         setArray([])
+        setProgressArray([])
+        setDoneArray([])
     }
 
     return (
@@ -27,7 +50,8 @@ export function List() {
                                 return (
                                     <tr key={index}>
                                         <td>{item}</td>
-                                        <td onClick={() => { removeItem(index) }}>remove</td>
+                                        <td onClick={() => { removeItem(index, itemArray, 1) }} className={style.listButton}>➖</td>
+                                        <td onClick={() => { moveItem(itemArray, progressArray, index, setArray, setProgressArray) }} className={style.listButton}>➡️</td>
                                     </tr>
                                 )
                             })}
@@ -42,11 +66,50 @@ export function List() {
                         addItem('');
                     }}
                 >
-                    <input onChange={(event) => { addItem(event.target.value) }} value={newItem} className={style.newItem}/>
-                    <input type='submit' value='Add new' className={style.submitButton}/>
-                    <input type='button' onClick={() => deleteAll()} value='Delete all' className={style.deleteAll}/>
+                    <input onChange={(event) => { addItem(event.target.value) }} value={newItem} className={style.newItem} />
+                    <input type='submit' value='Add new' className={style.submitButton} />
+                    <input type='button' onClick={() => deleteAll()} value='Delete all' className={style.deleteAll} />
                 </form>
 
+            </div>
+
+            <div className={style.container}>
+                <h1>In Progress</h1>
+                <div className={style.itemsContainer}>
+                    <table>
+                        <tbody>
+                            {progressArray.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{item}</td>
+                                        <td onClick={() => { moveItem(progressArray, itemArray, index, setProgressArray, setArray) }} className={style.listButton}>⬅️</td>
+                                        <td onClick={() => { removeItem(index, progressArray, 2) }} className={style.listButton}>➖</td>
+                                        <td onClick={() => { moveItem(progressArray, doneArray, index, setProgressArray, setDoneArray) }} className={style.listButton}>➡️</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+
+            <div className={style.container}>
+                <h1>Done</h1>
+                <div className={style.itemsContainer}>
+                    <table>
+                        <tbody>
+                            {doneArray.map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{item}</td>
+                                        <td onClick={() => { moveItem(doneArray, progressArray, index, setDoneArray, setProgressArray)}} className={style.listButton}>⬅️</td>
+                                        <td onClick={() => { removeItem(index, doneArray, 3) }} className={style.listButton}>➖</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     )
