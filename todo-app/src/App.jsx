808cd15c44e2f7135665441cdec9da73d90a7202
simo994q <simonkn1@hotmail.com>
2023-08-18 10:48:44 +0200

    delete test

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,12 +1,10 @@
 import { List } from './components/list/List'
-import { NewList } from './components/newList/NewList'
 
 function App() {
 
   return (
     <>
       <List />
-      {/* <NewList /> */}
     </>
   )
 }
