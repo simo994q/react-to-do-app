4c9cb36354bee1cf6706efb4f3b38714c400fb1c
simo994q <simonkn1@hotmail.com>
2023-09-08 09:12:09 +0200

    publish setup

--- todo-app/vite.config.js
+++ todo-app/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './https://github.com/simo994q/react-to-do-app',
   plugins: [react()],
 })
